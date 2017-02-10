e01222f722d291f891bd32698c0ac4a6159d69e4
ktz <ktz@kakaoui-MacBook-Air-7.local>
2017-02-11 04:54:20 +0900

    add Mocha Support

---
+++ test/javascript/Base.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ktz on 2017. 2. 11..
+ */
+import assert from 'assert'
+
+describe('HelloWorld', function () {
+    it('Print HelloWorld', function () {
+        var value = 1
+        console.log('Hello World')
+        assert.equal(value, 1)
+    })
+})

0d556b5657098f05ab283e10fc2c1f232cacfe4b
ktz <ktz@kakaoui-MacBook-Air-8.local>
2017-02-24 18:17:05 +0900

    redux-thunk support

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,12 +4,18 @@
 import React from 'react';
 
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import AtlantisStore from './atlantisredux'
 import { Provider } from 'react-redux';
 import App from './App.jsx'
+import thunk from 'redux-thunk'
 
-let atlantisStore = createStore(AtlantisStore)
+let atlantisStore = createStore(
+    AtlantisStore,
+    compose(
+        applyMiddleware(thunk)
+    )
+)
 
 let appElement = document.getElementById('app')
 

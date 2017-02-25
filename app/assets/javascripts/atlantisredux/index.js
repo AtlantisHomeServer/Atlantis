e3ce029c414ccb537114c3b920f4cff03ac1ea83
ktz <ktz@kakaoui-MacBook-Air-8.local>
2017-02-25 22:05:03 +0900

    redux-thunk support

--- app/assets/javascripts/atlantisredux/index.js
+++ app/assets/javascripts/atlantisredux/index.js
@@ -4,13 +4,11 @@
 
 import * as XMPPClientInfoReducers from './xmpp/XMPPClientInfoReducers'
 import { combineReducers } from 'redux'
-import {routerReducer} from 'react-router-redux'
+import { routerReducer } from 'react-router-redux'
 
 const XMPPClientInfoStore = combineReducers(XMPPClientInfoReducers);
 
-const AtlantisStore = combineReducers({
+export const AtlantisStore = combineReducers({
     XMPPInfoStore: XMPPClientInfoStore,
     routing: routerReducer
-})
-
-export default AtlantisStore
+});
